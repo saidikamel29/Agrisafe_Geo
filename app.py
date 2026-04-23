9a9ff815f3dd32ae05daf6333ae2b25369874edb
ksaidi <saidi.kamel.29@gmail.com>
2026-04-23 15:42:17 +0100

    version 1

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def pageprincipal():
+
+    return render_template('pageprincipal.html')
+
+@app.route('/conect')
+def conect():
+
+    return render_template('conect.html')
+
+@app.route('/aboutus')
+def aboutus():
+
+    return render_template('aboutus.html')
+
+@app.route('/service')
+def service():
+
+    return render_template('service.html')
+
+@app.route('/acount/<name>')
+def profile(name):
+    return "HELLO (name)"
+
+if __name__ == "__main__":
+    app.run(debug=True)
